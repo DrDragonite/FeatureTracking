99a2660b520f4c54072530524aa2cb2048f3ed74
Dartini <kostrhun.michal@gmail.com>
2021-09-28 19:40:32 +0200

    feature trackers can now be added, transformed and deleted. sidebar can be used to see/modify their properties too

--- display image ui/main.py
+++ display image ui/main.py
@@ -1,7 +1,10 @@
 from math import exp
-from tkinter import *
+import tkinter as tk
+from tkinter import Button, Label, ttk
 from tkinter.scrolledtext import ScrolledText
+from tkinter.ttk import Separator
 from PIL import Image, ImageTk
+from tracker import Tracker
 
 class Console(ScrolledText):
 	def __init__(self, *args, **kwargs) -> None:
@@ -10,7 +13,7 @@ class Console(ScrolledText):
 		self.ch_count = 0
 
 		class ConsoleManager:
-			def __init__(self, widget: Text, old_stdout: sys.stdout):
+			def __init__(self, widget: tk.Text, old_stdout: sys.stdout):
 				self.console = widget
 				self.old_stdout = old_stdout
 				
@@ -34,9 +37,9 @@ class Console(ScrolledText):
 
 		sys.stdout = ConsoleManager(self, sys.stdout)
 
-class InfoText(Label):
+class InfoText(tk.Label):
 	def __init__(self, *args, **kwargs) -> None:
-		self.text = StringVar()
+		self.text = tk.StringVar()
 		super().__init__(textvariable=self.text, *args, **kwargs)
 		self.infodict = {}
 
@@ -51,6 +54,20 @@ class InfoText(Label):
 		for key, value in kwargs.items():
 			self.infodict[key] = value
 		self.update_info()
+
+class LabeledWidget(tk.Frame):
+	def __init__(self, master: tk.Widget, label: str, widget: tk.Widget, *args, **kwargs) -> None:
+		tk.Frame.__init__(self, master, *args, **kwargs)
+		self.label = tk.Label(self, text=label)
+		self.widget = type(widget)(self)
+		self.widget.config({key:val[-1] for (key, val) in widget.config().items() if key!="class"})
+		widget.destroy()
+
+		self.label.pack(side="left")
+		self.widget.pack(side="right", fill="x", expand=1)
+
+	def widget_bind(self, event: str, callback):
+		self.widget.bind(event, callback)
 
 class Selection:
 	def __init__(self, x1, y1, x2=None, y2=None, w=None, h=None) -> None:
@@ -73,11 +90,11 @@ class SelectionManager:
 		self.x2 = 0
 		self.y2 = 0
 
-	def create(self, canvas: Canvas, x: int, y: int, **kwargs) -> None:
+	def create(self, canvas: tk.Canvas, x: int, y: int, **kwargs) -> None:
 		self.shape_id = canvas.create_rectangle(x, y, x, y, **kwargs)
 		self.x1, self.y1, self.x2, self.y2 = (x, y, x, y)
 	
-	def adjust(self, canvas: Canvas, x: int, y: int) -> None:
+	def adjust(self, canvas: tk.Canvas, x: int, y: int) -> None:
 		if not self.shape_id: return
 		self.x2, self.y2 = (x, y)
 		rx1 = self.x1 if self.x1 < self.x2 else self.x2
@@ -86,7 +103,7 @@ class SelectionManager:
 		ry2 = self.y1 if self.y1 > self.y2 else self.y2
 		canvas.coords(self.shape_id, rx1, ry1, rx2, ry2)
 	
-	def delete(self, canvas: Canvas) -> tuple:
+	def delete(self, canvas: tk.Canvas) -> tuple:
 		canvas.delete(self.shape_id)
 		x1 = self.x1 if self.x1 < self.x2 else self.x2
 		y1 = self.y1 if self.y1 < self.y2 else self.y2
@@ -96,7 +113,7 @@ class SelectionManager:
 		self.__init__()
 		return coords
 
-class Switch:
+class BoolSwitch:
 	def __init__(self) -> None:
 		self.state = False
 		self.onchange = None
@@ -119,6 +136,25 @@ class Switch:
 		self.state = not self.state
 		if self.onchange: self.onchange(self.state)
 
+class ObjectSwitch:
+	def __init__(self) -> None:
+		self.object = None
+		self.onchange = None
+
+	def __len__(self) -> bool:
+		return not not self.object
+
+	def __repr__(self) -> str:
+		return repr(self.object)
+
+	def __eq__(self, other) -> bool:
+		return self.object == other
+
+	def set(self, selected_object: object):
+		if self.object != selected_object:
+			if self.onchange: self.onchange(selected_object)
+		self.object = selected_object
+
 class TransformSwitch:
 	def __init__(self) -> None:
 		self.state = "none"
@@ -131,12 +167,20 @@ class TransformSwitch:
 	def __eq__(self, other) -> bool:
 		return self.state == str(other).lower()
 	
-	def set(self, state: str):
+	def set(self, state: str) -> None:
 		state = str(state).lower()
 		if not state in self.states: return 
 		if self.state != state:
 			if self.onchange: self.onchange(state)
 		self.state = state
+
+	def inverted(self) -> str:
+		inv_state = self.state
+		if   "top"    in self.state: inv_state.replace("top", "bottom")
+		elif "bottom" in self.state: inv_state.replace("bottom", "top")
+		if   "left"   in self.state: inv_state.replace("left", "right")
+		elif "right"  in self.state: inv_state.replace("right", "left")
+		return inv_state
 	
 	def to_cursor(self):
 		return ["fleur", "size_nw_se", "size_ns", "size_ne_sw", "size_we", "size_nw_se", "size_ns", "size_ne_sw", "size_we", ""][self.states.index(self.state)]
@@ -155,14 +199,14 @@ class TransformManager:
 		self.ox            = 0
 		self.oy            = 0
 	
-	def create(self, canvas: Canvas, x1: int, y1: int, x2: int, y2: int) -> None:
+	def create(self, canvas: tk.Canvas, x1: int, y1: int, x2: int, y2: int) -> None:
 		self.x1, self.y1, self.x2, self.y2 = (x1, y1, x2, y2)
 		self.main_rect = canvas.create_rectangle(0, 0, 0, 0, outline="#558ef0", fill=None)
 		self.center_marker = [canvas.create_line(0, 0, 0, 0, fill="#558ef0") for i in range(2)]
 		self.handles = [canvas.create_rectangle(0, 0, 0, 0, outline="#558ef0", fill="#ffffff") if i != 4 else None for i in range(9)]
 		self.update(canvas)
 
-	def update(self, canvas: Canvas) -> None:
+	def update(self, canvas: tk.Canvas) -> None:
 		"""Update the objects on the canvas."""
 		if not self.center_marker or not self.main_rect or not self.handles:
 			raise Exception("Please call 'create' function first")
@@ -190,11 +234,11 @@ class TransformManager:
 			y = int(i / 3)
 			#w = 6 + (1 * ((i+1) % 2))  # make corners bigger
 			w = 7
-			hx = rx1 + x * abs(rx2-rx1)/2
-			hy = ry1 + y * abs(ry2-ry1)/2
+			hx = rx1 + x * (rx2-rx1)/2
+			hy = ry1 + y * (ry2-ry1)/2
 			canvas.coords(self.handles[i], hx - w/2, hy - w/2, hx + w/2, hy + w/2)
 	
-	def delete(self, canvas: Canvas) -> None:
+	def delete(self, canvas: tk.Canvas) -> None:
 		"""Delete and reset the transform manager."""
 		if self.main_rect: canvas.delete(self.main_rect)
 		for i in range(9):
@@ -202,7 +246,7 @@ class TransformManager:
 			if i != 4 and self.handles: canvas.delete(self.handles[i])
 		self.__init__()
 	
-	def adjust(self, canvas: Canvas, mode: TransformSwitch, x: int, y: int) -> None:
+	def adjust(self, canvas: tk.Canvas, mode: TransformSwitch, x: int, y: int) -> None:
 		if "top" in mode.state:
 			self.y1 = y
 		if "right" in mode.state:
@@ -212,8 +256,8 @@ class TransformManager:
 		if "left" in mode.state:
 			self.x1 = x
 		if "move" in mode.state:
-			w  = self.x2 - self.x1
-			h  = self.y2 - self.y1
+			w = self.x2 - self.x1
+			h = self.y2 - self.y1
 			self.x1 = x - self.ox
 			self.y1 = y - self.oy
 			self.x2 = self.x1 + w
@@ -225,27 +269,21 @@ class TransformManager:
 		self.ox = x - self.x1
 		self.oy = y - self.y1
 
-	def reset_coords(self) -> None:
-		"""Order coordinates properly, making sure (x1, y1) is the top left corner."""
-		if self.x1 > self.x2:
-			self.x2 = self.x2 + self.x1
-			self.x1 = self.x2 - self.x1
-			self.x2 = self.x2 - self.x1
-		if self.y1 > self.y2:
-			self.y2 = self.y2 + self.y1
-			self.y1 = self.y2 - self.y1
-			self.y2 = self.y2 - self.y1
+	def reset_coords(self):
+		self.x1, self.y1, self.x2, self.y2 = self.coords()
 
 	def coords(self) -> tuple:
-		"""Get the transformation coordinates: (x1, y1, x2, y2)"""
-		self.reset_coords()
-		return (self.x1, self.y1, self.x2, self.y2)
+		"""Get the transformation coordinates: (x1, y1, x2, y2) in the proper order."""
+		x1, y1, x2, y2 = (self.x1, self.y1, self.x2, self.y2)
+		if x1 > x2: x2 = x2 + x1; x1 = x2 - x1; x2 = x2 - x1
+		if y1 > y2: y2 = y2 + y1; y1 = y2 - y1; y2 = y2 - y1
+		return (x1, y1, x2, y2)
 
 class ChooseImage:
 	def __init__(self) -> None:
 		self.image = 0
 	
-	def __new__(self) -> PhotoImage:
+	def __new__(self) -> ImageTk.PhotoImage:
 		from PIL import Image, ImageTk
 		from tkinter import filedialog
 		filepath = filedialog.askopenfilename(initialdir=folder+"src", title="Load an image", filetypes=(("Image files", ("*.png", "*.gif*", "*.jpg", "*.jpeg")), ("All files", ("*.*"))))
@@ -255,10 +293,12 @@ class ChooseImage:
 
 folder = __file__[:__file__.rfind("/")+1]
 
-class UI(Frame):
-	def __init__(self, root: Tk, *args, **kwargs):
-		Frame.__init__(self, root, *args, **kwargs)
-		self.pack(fill=BOTH, expand=1, padx=2, pady=2)
+trackers = []
+
+class UI(tk.Frame):
+	def __init__(self, root: tk.Tk, *args, **kwargs):
+		tk.Frame.__init__(self, root, *args, **kwargs)
+		self.pack(fill="both", expand=1, padx=2, pady=2)
 		self.root = root
 
 		self.root.title("Image GUI")
@@ -266,23 +306,24 @@ class UI(Frame):
 		self.root.resizable(0, 0)
 		#root.wm_attributes("-toolwindow", "true")
 
-		self.transform_state = Switch()
+		self.transform_object = ObjectSwitch()
+		self.transform_state = BoolSwitch()
 		self.transform_mode = TransformSwitch()
-		self.marker_state = Switch()
+		self.marker_state = BoolSwitch()
 		self.m_movement = 0
 		self.create_ui()
 
 	def create_ui(self):
-		self.footer = Frame(self, relief="flat")
+		self.footer = tk.Frame(self, relief="flat")
 		self.footer.pack(side="bottom", fill="x")
 		
-		self.sideframe = Frame(self, relief="groove", borderwidth=2, pady=10)
-		self.sideframe.pack(side="left", fill="y", ipadx=10)
+		self.sidebar = tk.Frame(self, relief="groove", borderwidth=2, pady=10)
+		self.sidebar.pack(side="left", fill="y", ipadx=15)
 
-		self.mainframe = Frame(self, relief="flat")
+		self.mainframe = tk.Frame(self, relief="flat")
 		self.mainframe.pack(side="right", fill="both", expand=1)
 
-		self.displayframe = Frame(self.mainframe, relief="groove", borderwidth=2)
+		self.displayframe = tk.Frame(self.mainframe, relief="groove", borderwidth=2)
 		self.displayframe.pack(side="top", fill="both", expand=1, anchor="n")
 
 		self.console = Console(self.mainframe, bg="#0f0f0f", fg="white", height=6)
@@ -297,49 +338,99 @@ class UI(Frame):
 		self.i_coords.set(x=0, y=0)
 
 		self.marker_state.onchange = self.change_marker_button_state
-		self.draw_marker_button = Button(self.sideframe, text="Draw marker", command=self.marker_state.toggle)
+		self.draw_marker_button = tk.Button(self.sidebar, text="Draw marker", command=self.marker_state.toggle)
 		self.draw_marker_button.pack(side="top", pady=5)
 
-		self.load_image_button = Button(self.sideframe, text="Load image", command=self.load_image_to_canvas)
+		self.load_image_button = tk.Button(self.sidebar, text="Load image", command=self.load_image_to_canvas)
 		self.load_image_button.pack(side="top", pady=5)
 
-	def remove_focus(self):
+		separator = Separator(self.sidebar, orient="horizontal")
+		separator.pack(fill="x", padx=5, pady=10)
+
+		self.transform_object.onchange = self.display_object_properties
+		self.root.bind("<Delete>", self.delete_object)
+		self.prop_objects = []
+
+	def remove_focus(self, *args, **kwargs):
 		"""Remove focus from a text area."""
 		self.root.focus_set()
 
 	def load_image_to_canvas(self):
 		img = ChooseImage() # needs to be a class because it doesn't work otherwise
 		if not img: return
-		self.canvas.create_image(self.canvas.winfo_width()/2, self.canvas.winfo_height()/2, image=img, anchor="center")
+		i = self.canvas.create_image(self.canvas.winfo_width()/2, self.canvas.winfo_height()/2, image=img, anchor="center")
+		self.canvas.tag_lower(i)
 
 	def change_marker_button_state(self, state):
 		# change button style
 		if state: self.draw_marker_button.config(fg="red", relief="sunken")
 		else: self.draw_marker_button.config(fg="black", relief="raised")
 
-	def setup_canvas(self, parent: Widget):
+	def delete_object(self, *args, **kwargs):
+		obj = self.transform_object.object
+		if isinstance(obj, Tracker):
+			if obj in trackers: trackers.remove(obj)
+			obj.tk_undraw(self.canvas)
+		self.canvas_transform.delete(self.canvas)
+		self.transform_state.set(False)
+		self.transform_object.set(None)
+		self.canvas.config(cursor="")
+	
+	def display_object_properties(self, object_):
+		def clear(objects: list):
+			for i in objects: i.destroy()
+		
+		def validate_number(input_):
+			if (input_.isdigit() and int(input_) < 1000) or input_ == "": return True
+			return False
+
+		prop = self.prop_objects
+		if isinstance(object_, Tracker):
+			def set_tracker_bg(var):
+				self.transform_object.object.set(bg_margin=int(var.get() or "0"))
+				self.transform_object.object.tk_draw(self.canvas)
+
+			clear(prop)
+			var = tk.StringVar()
+			var.set(str(object_.bg_margin))
+			var.trace("w", lambda name, index, mode, var=var: set_tracker_bg(var))
+			prop.append(LabeledWidget(self.sidebar, "BG size: ", ttk.Spinbox(self.sidebar, textvariable=var, from_=0, to=999, width=0, validate="key", validatecommand=(self.root.register(validate_number), "%P"))))
+			prop[-1].widget_bind("<Return>", self.remove_focus)
+			prop[-1].widget_bind("<Escape>", self.remove_focus)
+			prop[-1].pack(fill="x")
+
+			prop.append(tk.Button(self.sidebar, text="Delete", fg="red", command=self.delete_object))
+			prop[-1].pack(pady=20)
+		else:
+			clear(prop)
+
+	def setup_canvas(self, parent: tk.Widget):
 		def coords(event):
 			"""Get X and Y coordinates from a mouse event."""
 			return (int(event.x), int(event.y))
 		
-		def point_intersects_square_xyw(x, y, square_x, square_y, square_rad):
+		def point_intersects_square_center_xyw(x, y, square_x, square_y, square_rad):
 			"""Return whether a point (x, y) intersects with a square centered at (square_x, square_y) with size (2 * square_rad)"""
 			return x >= square_x - square_rad and x <= square_x + square_rad and y >= square_y - square_rad and y <= square_y + square_rad
+		
+		def point_intersects_rect_xywh(x, y, rect_x, rect_y, rect_width, rect_height):
+			"""Return whether a point (x, y) intersects with a rectangle positioned at (rect_x, rect_y) with size (rect_width, rect_height)"""
+			return x >= rect_x and x <= rect_x + rect_width and y >= rect_y and y <= rect_y + rect_height
 
 		def point_intersects_rect_xyxy(x, y, rect_x1, rect_y1, rect_x2, rect_y2):
 			"""Returns whether a point (x, y) intersects with a given rectangle (rect_x1, rect_y1, rect_x2, rect_y2)"""
 			return x >= rect_x1 and x <= rect_x2 and y >= rect_y1 and y <= rect_y2
 
-		def set_mode(x, y):
+		def set_transform_mode(x, y):
 			select_rad = 10
 			x1, y1, x2, y2 = self.canvas_transform.coords()
-			if   point_intersects_square_xyw(x, y, x1, y1, select_rad): # top left
+			if   point_intersects_square_center_xyw(x, y, x1, y1, select_rad): # top left
 				self.transform_mode.set("topleft")
-			elif point_intersects_square_xyw(x, y, x2, y1, select_rad): # top right
+			elif point_intersects_square_center_xyw(x, y, x2, y1, select_rad): # top right
 				self.transform_mode.set("topright")
-			elif point_intersects_square_xyw(x, y, x1, y2, select_rad): # bottom left
+			elif point_intersects_square_center_xyw(x, y, x1, y2, select_rad): # bottom left
 				self.transform_mode.set("bottomleft")
-			elif point_intersects_square_xyw(x, y, x2, y2, select_rad): # bottom right
+			elif point_intersects_square_center_xyw(x, y, x2, y2, select_rad): # bottom right
 				self.transform_mode.set("bottomright")
 			elif point_intersects_rect_xyxy(x, y, x1, y1 - select_rad, x2, y1 + select_rad): # top
 				self.transform_mode.set("top")
@@ -354,25 +445,53 @@ class UI(Frame):
 			else:
 				self.transform_mode.set("none")
 
+		def select_object(x, y):
+			for i, t in enumerate(trackers):
+				if point_intersects_rect_xywh(x, y, t.x, t.y, t.width, t.height):
+					trackers.insert(0, trackers.pop(i))
+					self.transform_object.set(t)
+					self.canvas_transform.create(self.canvas, t.x, t.y, t.x+t.width, t.y+t.height)
+					self.transform_state.set(True)
+					return
+			self.transform_object.set(None)
+
+		def deselect_object():
+			if self.transform_object != None:
+				if isinstance(self.transform_object.object, Tracker):
+					self.transform_object.object.tk_draw(self.canvas)
+					trackers.append(self.transform_object.object)
+			self.transform_object.set(None)
+
+		def apply_transformations():
+			if isinstance(self.transform_object.object, Tracker):
+				x1, y1, x2, y2 = self.canvas_transform.coords()
+				self.transform_object.object.set(x=x1, y=y1, width=x2-x1, height=y2-y1)
+				self.transform_object.object.tk_draw(self.canvas)
+
 		def mouse_click(event):
+			global trackers
 			# get event coords
 			x, y = coords(event)
 			self.canvas.mouse_down = True
 			if self.transform_state:
-				# if user clicks away, remove the transform
+				# if user clicks away from the transform, remove the transformation manager
 				if self.transform_mode == "none":
-					self.transform_state.set(False)
+					deselect_object()
 					self.canvas_transform.delete(self.canvas)
-				set_mode(x, y)
+					self.transform_state.set(False)
+				# manage the transformation manager
+				set_transform_mode(x, y)
 				self.canvas_transform.set_offset(x, y)
 				self.canvas.config(cursor=self.transform_mode.to_cursor())
 			if self.marker_state:
+				# create a selection to vizualize where the marker is being made
 				self.canvas_selection.create(self.canvas, x, y, outline="red", dash=(3, 5), fill=None)
+			if not self.transform_state and not self.marker_state:
+				select_object(x, y)
 
 		def mouse_move(event):
-			# limit the amount of times the event is executed
 			self.m_movement += 1
-			if self.m_movement % 2: return
+			if self.m_movement % 5: return
 			# get event coords
 			x, y = coords(event)
 			# update the footer info
@@ -381,9 +500,10 @@ class UI(Frame):
 				# if the mouse is held down, perform the scaling transformation
 				if self.canvas.mouse_down:
 					self.canvas_transform.adjust(self.canvas, self.transform_mode, x, y)
+					apply_transformations()
 				# if it's not, just update the cursor
 				if not self.canvas.mouse_down:
-					set_mode(x, y)
+					set_transform_mode(x, y)
 					self.canvas.config(cursor=self.transform_mode.to_cursor())
 			if self.marker_state and self.canvas.mouse_down:
 				self.canvas_selection.adjust(self.canvas, x, y)
@@ -393,25 +513,28 @@ class UI(Frame):
 			self.canvas.mouse_down = False
 			self.remove_focus()
 			if self.transform_state:
-				set_mode(x, y)
+				set_transform_mode(x, y)
+				self.canvas_transform.reset_coords()
 				self.canvas.config(cursor=self.transform_mode.to_cursor())
 			if self.marker_state:
 				# use the selection coordinates to create a transformer
 				x1, y1, x2, y2 = self.canvas_selection.delete(self.canvas)
 				self.marker_state.set(False)
+				self.transform_object.set(Tracker(x1, y1, x2-x1, y2-y1, mode="TOPLEFT"))
+				self.transform_object.object.tk_draw(self.canvas)
 				self.canvas_transform.create(self.canvas, x1, y1, x2, y2)
 				self.transform_state.set(True)
 		
 		# set up the canvas and local variables
 		self.canvas_selection = SelectionManager()
 		self.canvas_transform = TransformManager()
-		self.canvas = Canvas(parent, bg="white")
+		self.canvas = tk.Canvas(parent, bg="white")
 		self.canvas.pack(fill="both", expand=1)
 		self.canvas.bind("<Motion>", mouse_move)
 		self.canvas.bind("<Button-1>", mouse_click)
 		self.canvas.bind("<ButtonRelease-1>", mouse_release)
 		self.canvas.mouse_down = False
 
-root = Tk()
+root = tk.Tk()
 app = UI(root)
 root.mainloop()
--- display image ui/tracker.py
+++ display image ui/tracker.py
@@ -5,6 +5,8 @@ MODES  = ("TOPLEFT", "CENTER")
 class Tracker:
 	def __init__(self, x: float, y: float, width: float, height: float, *, bg_margin: float = 10, mode: str = "CENTER") -> None:
 		self.set(x=x, y=y, width=width, height=height, bg_margin=bg_margin, mode=mode)
+		self.fg_rect = None
+		self.bg_rect = None
 
 	def set(self, x=None, y=None, width=None, height=None, bg_margin=None, mode=None) -> None:
 		self.x         = x         if isinstance(x, NUMBER)         else self.x
@@ -14,21 +16,29 @@ class Tracker:
 		self.bg_margin = bg_margin if isinstance(bg_margin, NUMBER) else self.bg_margin
 		self.bg_width  = self.width  + self.bg_margin
 		self.bg_height = self.height + self.bg_margin
-		if mode and not isinstance(mode, str) and not mode.upper() in MODES:
+		if mode and (not isinstance(mode, str) or not mode.upper() in MODES):
 			raise Exception("Invalid mode")
 		self.mode = mode if mode else self.mode
 
-	def get_fg(self, pixels: list, image_width: int, image_height: int) -> list:
-		from util import constrain
+	def tk_draw(self, canvas):
+		if not self.fg_rect: self.fg_rect = canvas.create_rectangle(0, 0, 0, 0, outline="#00ff00", fill=None)
+		if not self.bg_rect: self.bg_rect = canvas.create_rectangle(0, 0, 0, 0, outline="#00ff00", fill=None, dash=(3, 5))
+		x1, y1, x2, y2 = self.fg_coords()
+		canvas.coords(self.fg_rect, x1, y1, x2, y2)
+		x1, y1, x2, y2 = self.bg_coords()
+		canvas.coords(self.bg_rect, x1, y1, x2, y2)
 
+	def tk_undraw(self, canvas):
+		if self.fg_rect: canvas.delete(self.fg_rect)
+		if self.bg_rect: canvas.delete(self.bg_rect)
+
+	def fg_coords(self):
 		self.set()
-		foreground: list = []
-
 		# calculate offsets
 		x_off = 0
 		y_off = 0
 		if self.mode == "CENTER":
-			x_off = self.width  / 2
+			x_off = self.width / 2
 			y_off = self.height / 2
 
 		# calculate coordinates with offset applied
@@ -37,11 +47,38 @@ class Tracker:
 		x2_o = x1_o + self.width
 		y2_o = y1_o + self.height
 
+		# return the coordinates
+		return (x1_o, y1_o, x2_o, y2_o)
+
+	def bg_coords(self):
+		self.set()
+		# calculate offsets
+		x_off = 0
+		y_off = 0
+		if self.mode == "CENTER":
+			x_off = self.bg_width
+			y_off = self.bg_height
+
+		# calculate coordinates with offset applied
+		x1_o = self.x - self.bg_margin - x_off
+		y1_o = self.y - self.bg_margin - y_off
+		x2_o = x1_o + self.bg_width  + self.bg_margin
+		y2_o = y1_o + self.bg_height + self.bg_margin
+
+		# return the coordinates
+		return (x1_o, y1_o, x2_o, y2_o)
+
+	def get_fg(self, pixels: list, image_width: int, image_height: int) -> list:
+		from util import constrain
+
+		foreground: list = []
+
 		# calculate border-constrained coordinates
-		x1_c = constrain(x1_o, 0, image_width)
-		y1_c = constrain(y1_o, 0, image_height)
-		x2_c = constrain(x2_o, 0, image_width)
-		y2_c = constrain(y2_o, 0, image_height)
+		x1, y1, x2, y2 = self.fg_coords()
+		x1_c = constrain(x1, 0, image_width)
+		y1_c = constrain(y1, 0, image_height)
+		x2_c = constrain(x2, 0, image_width)
+		y2_c = constrain(y2, 0, image_height)
 
 		for y in range(y1_c, y2_c):
 			# get the edges of a horizontal "strip" of the foreground
@@ -57,27 +94,14 @@ class Tracker:
 	def get_bg(self, pixels: list, image_width: int, image_height: int) -> list:
 		from util import constrain
 
-		self.set()
 		background: list = []
 
-		# calculate offsets
-		x_off = 0
-		y_off = 0
-		if self.mode == "CENTER":
-			x_off = (self.bg_width)  / 2
-			y_off = (self.bg_height) / 2
-
-		# calculate coordinates with offset applied
-		x1_o = self.x - x_off
-		y1_o = self.y - y_off
-		x2_o = x1_o + self.bg_width
-		y2_o = y1_o + self.bg_height
-
 		# calculate border-constrained coordinates
-		x1_c = constrain(x1_o, 0, image_width)
-		y1_c = constrain(y1_o, 0, image_height)
-		x2_c = constrain(x2_o, 0, image_width)
-		y2_c = constrain(y2_o, 0, image_height)
+		x1, y1, x2, y2 = self.bg_coords()
+		x1_c = constrain(x1, 0, image_width)
+		y1_c = constrain(y1, 0, image_height)
+		x2_c = constrain(x2, 0, image_width)
+		y2_c = constrain(y2, 0, image_height)
 
 		for y in range(y1_c, y2_c):
 			# get the edges of a horizontal "strip" of the background
